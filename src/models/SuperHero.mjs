8b9413abbf06f1c6cca9203a7f10e78a92c5cad4
PaoGabiVilla <pagavi161179@gmail.com>
2025-03-10 12:39:28 -0300

    Agregado: Esquema de Superheroes.

---
+++ src/models/SuperHero.mjs
@@ -0,0 +1,15 @@
+import mongoose from 'mongose';
+const superheroSchema = new mongoose.Schema({
+    nombreSuerHero: {type: String, required: true},
+    nombreReal: {type: String, required: true},
+    edad: {type: Number, min: 0},
+    planetaOrigen: { type: String, default: 'Desconocido'},
+    debilidad: String,
+    poderes:[String],
+    aliados:[String],
+    enemigos:[String],
+    creador: String,
+    createdAt: { type: Date, default: Date.now}
+})
+const superHero = mongoose.model('SuperHero', superheroSchema, 'Grupo-06');
+export default superHero
