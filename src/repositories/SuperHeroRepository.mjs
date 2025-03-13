bd26dd2e946a436530be5a9cebb23a5c5d46fbf9
paogabivilla <pagavi161179@gmail.com>
2025-03-13 06:37:40 -0300

    modificar errores

--- src/repositories/SuperHeroRepository.mjs
+++ src/repositories/SuperHeroRepository.mjs
@@ -13,21 +13,20 @@ class SuperHeroRepository extends IRepository {
     async buscarPorAtributo(atributo, valor) {
         // RESOLVER
         
-     /* //Se declara una variable llamada filtrar que es un objeto vacío {}.Este objeto servirá como filtro para la consulta en MongoDB.
+      //Se declara una variable llamada filtrar que es un objeto vacío {}.Este objeto servirá como filtro para la consulta en MongoDB.
         const filtrar = {};
-        //$regex: valor: "permite buscar valores que contengan la palabra, sin importar en qué parte del texto aparezca."
-        //$options: 'i': Hace la búsqueda insensible a mayúsculas y minúsculas.
+        
         filtrar[atributo] = {$regex: valor,$options:'i'};//Búsqueda insencible a mayúsculas
-        return await superHero.find(filtrar);*/
+        return await superHero.find(filtrar);
 
     }
 
     async obtenerMayoresDe30() {
         // RESOLVER
-        /*{ edad: { $gt: 30 } }:Es el filtro de búsqueda en MongoDB.
-        { $gt: 30 }:$gt (greater than) es un operador de MongoDB que significa "mayor que".*/
+       /* { edad: { $gt: 30 }
+        { $gt: 30 }:$gt //(greater than) es un operador de MongoDB que significa "mayor que".
         return await superHero.find({edad: {$gt:30}});
         }
-    }
+    }*/
 
 export default new SuperHeroRepository();
